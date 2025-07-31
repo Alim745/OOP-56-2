f1c25f6de49905b78453b76c24a6ae8c97d4623a
Alim745 <alimmaratov@hmail.com>
2025-07-31 22:09:11 +0600

    hw6 added

--- hw/hw6.py
+++ hw/hw6.py
@@ -3,7 +3,12 @@
 
 """
 
-import numpy
+import numpy # Импортируем numpy
 
+numbers = numpy.array([5, 10, 15, 20, 25]) # Создаем массив чисел
 
-
+print(f"Массив чисел: {numbers}")
+print(f"Минимум: {numpy.min(numbers)}") # Находим минимальное число
+print(f"Максимум: {numpy.max(numbers)}") # Находим максимальное число
+print(f"Сумма чисел: {numpy.sum(numbers)}") # Считаем сумму всех чисел
+print(f"Среднее значение: {numpy.mean(numbers)}") # Считаем среднее арифметическое
